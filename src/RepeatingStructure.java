25d9cadb12856e94aa6594c6bd1194594605a514
dell <vivekofficial2018@gmail.com>
2019-06-23 11:28:30 +0530

    repeating program added

---
+++ src/RepeatingStructure.java
@@ -0,0 +1,10 @@
+public class RepeatingStructure {
+    public static void main(String[] args) {
+        for (int i = 1; i <7 ; i++) {
+            if(i%3==0)
+                System.out.println("tok");
+            else
+                System.out.println("tik");
+        }
+    }
+}
